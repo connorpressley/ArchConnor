2ceb2a4eea644266c60cb7d2103396de1f3a0154
connorpressley <78635569+connorpressley@users.noreply.github.com>
2021-12-21 12:17:25 -0600

    typo

--- 2-user.sh
+++ 2-user.sh
@@ -57,9 +57,9 @@ for PKG in "${PKGS[@]}"; do
 done
 
 export PATH=$PATH:~/.local/bin
-cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
+cp -r $HOME/ArchConnor/dotfiles/* $HOME/.config/
 pip install konsave
-konsave -i $HOME/ArchTitus/kde.knsv
+konsave -i $HOME/ArchConnor/kde.knsv
 sleep 1
 konsave -a kde
 
